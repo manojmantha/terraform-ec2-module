32cefe098d1bf62c8c45064312b11ca03693093f
manoj515876 <manojputtaluckey@gmail.com>
2026-01-29 11:11:23 +0530

    terraform ec2 module creation

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "sg_id" {
+
+}
+
+variable "tags" {
+  default = {}
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    validation {
+        condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+        error_message = "Valid values for instance type are: t3.small t3.medium t3.micro"
+    } 
+}
